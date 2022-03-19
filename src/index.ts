652f0ced05e9128fe6fa996a84403817307a4c47
mhp <hpousty@gmail.com>
2022-03-19 19:59:49 +0330

    removing spacer, index.ts in components and updating global index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,16 @@ export {default as Theme} from './core/theme';
 export {default as withTheme} from './core/withTheme';
 export {ThemeProvider, ThemePrefrencesContext} from './core/provider';
 
-export * from './components';
 export * from './core/types';
+
+export {Text} from './components/Text';
+export {Icon} from './components/Icon';
+export {Left} from './components/Left';
+export {Right} from './components/Right';
+export {Center} from './components/Center';
+export {Header} from './components/Header';
+export {Divider} from './components/Divider';
+export {Wrapper} from './components/Wrapper';
+export {Content} from './components/Content';
+export {Touchable} from './components/Touchable';
+export {Container} from './components/Container';
